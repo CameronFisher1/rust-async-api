2c0405a481347194ec9e26e66e80f48a8ce4685a
Cameron Fisher <camlucfis@gmail.com>
2026-03-21 16:02:20 -0400

    Add error handling + error responses

--- src/controller/user.rs
+++ src/controller/user.rs
@@ -3,13 +3,23 @@ use axum::http::StatusCode;
 use axum::Json;
 use uuid::Uuid;
 use crate::AppState;
+use crate::model::error::ErrorRes;
 use crate::model::user::{CreateUserRequest, UpdateUserRequest, User};
 
 pub async fn create_user(
     State(state): State<AppState>,
     Json(payload): Json<CreateUserRequest>
-) -> Result<(StatusCode, Json<User>), StatusCode> {
-    let mut users = state.users.lock().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+) -> Result<(StatusCode, Json<User>), (StatusCode, Json<ErrorRes>)> {
+    let mut users = state.users.lock()
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, Json(ErrorRes {
+            error: "Internal Server Error".to_string(),
+        })))?;
+
+    if payload.name.is_empty() || payload.description.is_empty() {
+        return Err((StatusCode::BAD_REQUEST, Json(ErrorRes {
+            error: "Invalid input".to_string()
+        })));
+    }
 
     let id = Uuid::new_v4().to_string();
 
@@ -18,7 +28,7 @@ pub async fn create_user(
         name: payload.name,
         description: payload.description,
     };
-    // users.push(user.clone());
+
     users.insert(id.clone(), user.clone());
 
     println!("user {} created", id);
@@ -28,8 +38,11 @@ pub async fn create_user(
 
 pub async fn get_all_users(
     State(state): State<AppState>
-) -> Result<(StatusCode, Json<Vec<User>>), StatusCode> {
-    let users = state.users.lock().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+) -> Result<(StatusCode, Json<Vec<User>>), (StatusCode, Json<ErrorRes>)> {
+    let users = state.users.lock()
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, Json(ErrorRes {
+            error: "Internal Server Error".to_string(),
+        })))?;
 
     Ok((StatusCode::OK, Json(users.values().cloned().collect())))
 }
@@ -37,13 +50,24 @@ pub async fn get_all_users(
 pub async fn get_user(
     State(state): State<AppState>,
     Path(id): Path<String>
-) -> Result<(StatusCode, Json<User>), StatusCode> {
-    let users = state.users.lock().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+) -> Result<(StatusCode, Json<User>), (StatusCode, Json<ErrorRes>)> {
+    let users = state.users.lock()
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, Json(ErrorRes {
+            error: "Internal Server Error".to_string(),
+        })))?;
+
+    if !is_valid_uuid(&id) {
+        return Err((StatusCode::BAD_REQUEST, Json(ErrorRes {
+            error: "Invalid ID".to_string()
+        })));
+    }
 
     let user = users.get(&id);
 
     if user.is_none() {
-        Err(StatusCode::NOT_FOUND)
+        Err((StatusCode::NOT_FOUND, Json(ErrorRes {
+            error: "User not found".to_string()
+        })))
     } else {
         Ok((StatusCode::OK, Json(user.unwrap().clone())))
     }
@@ -53,11 +77,22 @@ pub async fn update_user(
     State(state): State<AppState>,
     Path(id): Path<String>,
     Json(payload): Json<UpdateUserRequest>
-) -> Result<(StatusCode, Json<User>), StatusCode> {
-    let mut users = state.users.lock().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+) -> Result<(StatusCode, Json<User>), (StatusCode, Json<ErrorRes>)> {
+    let mut users = state.users.lock()
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, Json(ErrorRes {
+            error: "Internal Server Error".to_string(),
+        })))?;
+
+    if !is_valid_uuid(&id) || payload.name.is_empty() || payload.description.is_empty() {
+        return Err((StatusCode::BAD_REQUEST, Json(ErrorRes {
+            error: "Invalid input".to_string()
+        })));
+    }
 
     if !users.contains_key(&id) {
-        return Err(StatusCode::NOT_FOUND)
+        return Err((StatusCode::NOT_FOUND, Json(ErrorRes {
+            error: "User not found".to_string()
+        })))
     }
 
     let new_user = User {
@@ -74,15 +109,33 @@ pub async fn update_user(
 pub async fn delete_user(
     State(state): State<AppState>,
     Path(id): Path<String>,
-) -> Result<StatusCode, StatusCode> {
-    let mut users = state.users.lock().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+) -> Result<StatusCode, (StatusCode, Json<ErrorRes>)> {
+    let mut users = state.users.lock()
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, Json(ErrorRes {
+            error: "Internal Server Error".to_string(),
+        })))?;
+
+    if !is_valid_uuid(&id) {
+        return Err((StatusCode::BAD_REQUEST, Json(ErrorRes {
+            error: "Invalid ID".to_string()
+        })));
+    }
 
     let initial_user_count = users.len();
     users.remove(&id);
 
     if users.len() == initial_user_count {
-        Err(StatusCode::NOT_FOUND)
+        Err((StatusCode::NOT_FOUND, Json(ErrorRes {
+            error: "User not found".to_string()
+        })))
     } else {
         Ok(StatusCode::NO_CONTENT)
+    }
+}
+
+fn is_valid_uuid(uuid_str: &str) -> bool {
+    match Uuid::parse_str(uuid_str) {
+        Ok(_) => true,
+        Err(_) => false,
     }
 }
